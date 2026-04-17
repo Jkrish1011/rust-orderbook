2081f7ec86d91bd710aecab0c573af922791ed89
Jayakrishnan <jayakrishnanashok@gmail.com>
2026-04-17 11:47:55 +0400

    bid + ask parsing

--- src/main.rs
+++ src/main.rs
@@ -30,7 +30,10 @@ fn main() -> Result<(), CustomError> {
         .map_err(|_| CustomError::ParseError("Couln't open the file".to_string()))?;
     // let s_idx = start_bytes.len();
     while let Ok(packet) = cap.next_packet() {
-        match SlicedPacket::from_ethernet(&packet.data) {
+        let ts_sec = packet.header.ts.tv_sec;
+        let ts_usec = packet.header.ts.tv_usec;
+
+        match SlicedPacket::from_ethernet(&packet.data) {    
             Ok(value) => {
                 match value.transport {
                     Some(etherparse::TransportSlice::Udp(udp)) => {
@@ -41,8 +44,36 @@ fn main() -> Result<(), CustomError> {
                         let Some(quote_packet) = QuotePacketView::try_new(&payload) else {
                             continue;
                         };
+                        // println!("{}.{:06}", ts_sec, ts_usec);
+                        let tz_offset_secs = 9 * 3600; // Expecting the packet was captured in a timezone which is KST. 9 hours ahead )
+                        let seconds_today = (ts_sec + tz_offset_secs) % 86400; 
+
+                        let packet_total_micros = (seconds_today as u64 * 1_000_000) + (ts_usec as u64);
+                        println!("Packet Time: {}", packet_total_micros);
                         println!("Issue Code: {:?}", quote_packet.issue_code());
                         println!("Accept Time: {:?}", quote_packet.accept_time());
+                        let (bid_price1, bid_qty1) = quote_packet.bid(0);
+                        let (bid_price2, bid_qty2) = quote_packet.bid(1);
+                        let (bid_price3, bid_qty3) = quote_packet.bid(2);
+                        let (bid_price4, bid_qty4) = quote_packet.bid(3);
+                        let (bid_price5, bid_qty5) = quote_packet.bid(4);
+                        println!("Bid 1: {:?}@{:?}", bid_price1, bid_qty1);
+                        println!("Bid 2: {:?}@{:?}", bid_price2, bid_qty2);
+                        println!("Bid 3: {:?}@{:?}", bid_price3, bid_qty3);
+                        println!("Bid 4: {:?}@{:?}", bid_price4, bid_qty4);
+                        println!("Bid 5: {:?}@{:?}", bid_price5, bid_qty5);
+                        println!("---");
+
+                        let (ask_price1, ask_qty1) = quote_packet.ask(0);
+                        let (ask_price2, ask_qty2) = quote_packet.ask(1);
+                        let (ask_price3, ask_qty3) = quote_packet.ask(2);
+                        let (ask_price4, ask_qty4) = quote_packet.ask(3);
+                        let (ask_price5, ask_qty5) = quote_packet.ask(4);
+                        println!("Ask 1: {:?}@{:?}", ask_price1, ask_qty1);
+                        println!("Ask 2: {:?}@{:?}", ask_price2, ask_qty2);
+                        println!("Ask 3: {:?}@{:?}", ask_price3, ask_qty3);
+                        println!("Ask 4: {:?}@{:?}", ask_price4, ask_qty4);
+                        println!("Ask 5: {:?}@{:?}", ask_price5, ask_qty5);
                         println!("---");
                         // if payload.starts_with(QUOTE_PREFIX) && (d_port == 15515 || d_port == 15516) {
 
--- src/packet.rs
+++ src/packet.rs
@@ -2,6 +2,11 @@ pub const QUOTE_PACKET_SIZE: usize = 215;
 pub const QUOTE_PREFIX: &[u8; 5] = b"B6034";
 pub const ISSUE_CODE_META: &[usize; 2] = &[5, 12]; // OFFSET_VALUE, LENGTH
 pub const ACCEPT_TIME_META: &[usize; 2] = &[206, 8];
+pub const BID_FIRST_PRICE_META: &[usize; 2] = &[29, 5];
+pub const BID_FIRST_QTY_META: &[usize; 2] = &[34, 7];
+pub const ASK_FIRST_PRICE_META: &[usize; 2] = &[96, 5];
+pub const ASK_FIRST_QTY_META: &[usize; 2] = &[101, 7];
+pub const LEVEL_STRIDE: usize = 12;
 pub const END_OF_PACKET: usize = 214;
 
 #[derive(Debug, Clone)]
@@ -20,20 +25,70 @@ impl<'a> QuotePacketView<'a> {
     }
 
     #[inline(always)]
+    pub fn parse_ascii_to_u32(bytes: &[u8]) -> u32 {
+        let mut acc: u32 = 0;
+        // bitwise operations are 1 cpu cycle
+        for &b in bytes {
+            // (b & 0x0F) converts the ASCII byte to an integer from 0-9
+            // acc * 10 shifts the current total to the left by one base-10 digit
+            acc = acc * 10 + (b & 0x0F) as u32;
+        }
+        
+        acc
+    }
+
+    #[inline(always)]
     pub fn issue_code(&self) -> &'a str {
         std::str::from_utf8(&self.raw[ISSUE_CODE_META[0]..(ISSUE_CODE_META[0] + ISSUE_CODE_META[1])]).unwrap()
     }
 
     #[inline(always)]
     pub fn accept_time(&self) -> u64 {
-        let time_ref = &self.raw[ACCEPT_TIME_META[0]..(ACCEPT_TIME_META[0]+ACCEPT_TIME_META[1])];
+        
+        // let time_ref1 = std::str::from_utf8(&self.raw[ACCEPT_TIME_META[0]..(ACCEPT_TIME_META[0]+ACCEPT_TIME_META[1])]).unwrap();
+        // let hh = time_ref1[0..2].parse::<u64>().unwrap();
+        // let mm = time_ref1[2..4].parse::<u64>().unwrap();
+        // let ss = time_ref1[4..6].parse::<u64>().unwrap();
+        // let uu = time_ref1[6..8].parse::<u64>().unwrap();
 
+        // // Formula: (hours * 3600 + minutes * 60 + seconds) * 1,000,000 + microseconds
+        // let t = (hh * 3600 + mm * 60 + ss) * 1_000_000 + uu;
+        let time_ref = &self.raw[ACCEPT_TIME_META[0]..(ACCEPT_TIME_META[0]+ACCEPT_TIME_META[1])];
+        
+        // Explore get_unchecked to unlock more speed
         let hh = (time_ref[0] as u64 - 48) * 10 + (time_ref[1] as u64 - 48);
         let mm = (time_ref[2] as u64 - 48) * 10 + (time_ref[3] as u64 - 48);
         let ss = (time_ref[4] as u64 - 48) * 10 + (time_ref[5] as u64 - 48);
         let uu = (time_ref[6] as u64 - 48) * 10 + (time_ref[7] as u64 - 48);
 
-        let total_secs = hh * 3600 + mm * 60 + ss;
-        (total_secs * 1_000_000) + (uu * 10_000)
+        // let total_secs = hh * 3600 + mm * 60 + ss;
+        // let t2 = (total_secs * 1_000_000) + (uu);
+        // println!("t1: {} || t2: {}", t, t2);
+        // ( hh * 3600 + mm * 60 + ss ) * 1_000_000 + uu
+        (hh * 3600 + mm * 60 + ss) * 1_000_000 + (uu * 10_000) // Normalize uu to micro seconds
+    }
+
+    #[inline(always)]
+    pub fn bid(&self, level: usize) -> (u32, u32) {
+        let base = level * LEVEL_STRIDE;
+        let price_start = BID_FIRST_PRICE_META[0] + base;
+        let qty_start = BID_FIRST_QTY_META[0] + base;
+
+        let price = Self::parse_ascii_to_u32(&self.raw[price_start..(price_start + BID_FIRST_PRICE_META[1])]);
+        let qty = Self::parse_ascii_to_u32(&self.raw[qty_start..(qty_start + BID_FIRST_QTY_META[1])]);
+        
+        (price, qty)
+    }
+
+    #[inline(always)]
+    pub fn ask(&self, level: usize) -> (u32, u32) {
+        let base = level * LEVEL_STRIDE;
+        let price_start = ASK_FIRST_PRICE_META[0] + base;
+        let qty_start = ASK_FIRST_QTY_META[0] + base;
+
+        let price = Self::parse_ascii_to_u32(&self.raw[price_start..(price_start + ASK_FIRST_PRICE_META[1])]);
+        let qty = Self::parse_ascii_to_u32(&self.raw[qty_start..(qty_start + ASK_FIRST_QTY_META[1])]);
+        
+        (price, qty)
     }
 }
