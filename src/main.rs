dd1b0034fea03b058c922e5077a0014be1170351
Jayakrishnan <jayakrishnanashok@gmail.com>
2026-04-18 23:51:06 +0400

    version 1

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,75 @@ mod packet;
 use anyhow::Result;
 use clap::Parser;
 use etherparse::{SlicedPacket, TransportSlice};
-use pcap::Capture;
+
 use std::path::Path;
 
+use memmap2::Mmap;
+use pcap_file::pcap::PcapReader;
+use std::cmp::Reverse;
+use std::collections::BinaryHeap;
+use std::fs::File;
+
 use crate::error::CustomError;
-use crate::packet::QuotePacketView;
+use crate::packet::{Quote, QuotePacketView};
 
 #[derive(Parser, Debug)]
 #[command(version, about)]
 struct Args {
     #[arg(short)]
     r: bool,
+}
+
+fn print_quote(quote: Quote) {
+    let (bid_price1, bid_qty1) = quote.quote.bid(0);
+    let (bid_price2, bid_qty2) = quote.quote.bid(1);
+    let (bid_price3, bid_qty3) = quote.quote.bid(2);
+    let (bid_price4, bid_qty4) = quote.quote.bid(3);
+    let (bid_price5, bid_qty5) = quote.quote.bid(4);
+    // println!("Bid 1: {:?}@{:?}", bid_price1, bid_qty1);
+    // println!("Bid 2: {:?}@{:?}", bid_price2, bid_qty2);
+    // println!("Bid 3: {:?}@{:?}", bid_price3, bid_qty3);
+    // println!("Bid 4: {:?}@{:?}", bid_price4, bid_qty4);
+    // println!("Bid 5: {:?}@{:?}", bid_price5, bid_qty5);
+    // println!("---");
+
+    let (ask_price1, ask_qty1) = quote.quote.ask(0);
+    let (ask_price2, ask_qty2) = quote.quote.ask(1);
+    let (ask_price3, ask_qty3) = quote.quote.ask(2);
+    let (ask_price4, ask_qty4) = quote.quote.ask(3);
+    let (ask_price5, ask_qty5) = quote.quote.ask(4);
+    // println!("Ask 1: {:?}@{:?}", ask_price1, ask_qty1);
+    // println!("Ask 2: {:?}@{:?}", ask_price2, ask_qty2);
+    // println!("Ask 3: {:?}@{:?}", ask_price3, ask_qty3);
+    // println!("Ask 4: {:?}@{:?}", ask_price4, ask_qty4);
+    // println!("Ask 5: {:?}@{:?}", ask_price5, ask_qty5);
+    println!(
+        "{:?} {:?} {:?} {:?}@{:?} {:?}@{:?} {:?}@{:?} {:?}@{:?} {:?}@{:?} {:?}@{:?} {:?}@{:?} {:?}@{:?} {:?}@{:?} {:?}@{:?}",
+        quote.pkt_time,
+        quote.accept_time,
+        quote.quote.issue_code(),
+        bid_qty5,
+        bid_price5,
+        bid_qty4,
+        bid_price4,
+        bid_qty3,
+        bid_price3,
+        bid_qty2,
+        bid_price2,
+        bid_qty1,
+        bid_price1,
+        ask_qty5,
+        ask_price5,
+        ask_qty4,
+        ask_price4,
+        ask_qty3,
+        ask_price3,
+        ask_qty2,
+        ask_price2,
+        ask_qty1,
+        ask_price1
+    );
+    println!("---");
 }
 
 fn main() -> Result<(), CustomError> {
@@ -26,14 +84,33 @@ fn main() -> Result<(), CustomError> {
         println!("Disabled");
     }
 
-    let mut cap = Capture::from_file(Path::new("./data/mdf-kospi200.20110216-0.pcap"))
-        .map_err(|_| CustomError::ParseError("Couln't open the file".to_string()))?;
-    // let s_idx = start_bytes.len();
-    while let Ok(packet) = cap.next_packet() {
-        let ts_sec = packet.header.ts.tv_sec;
-        let ts_usec = packet.header.ts.tv_usec;
+    let file = File::open("./data/mdf-kospi200.20110216-0.pcap")
+        .map_err(|e| CustomError::ParseError(format!("Error: {:?}", e)))?;
+    let mmap = unsafe {
+        Mmap::map(&file).map_err(|e| CustomError::ParseError(format!("Error: {:?}", e)))?
+    };
+    let data = &mmap[..];
 
-        match SlicedPacket::from_ethernet(&packet.data) {    
+    let mut cap = PcapReader::new(data)
+        .map_err(|e| CustomError::ParseError(format!("Pcap Header Error: {:?}", e)))?;
+
+    let mut heap = BinaryHeap::new();
+    let window_micros: u64 = 3_000_000; // 3 seconds
+
+    // let s_idx = start_bytes.len();
+    while let Some(Ok(packet)) = cap.next_packet() {
+        // let ts_sec = pkt.header.ts.tv_sec;
+        // let ts_usec = pkt.header.ts.tv_usec;
+
+        // let packet = pkt.unwrap();
+
+        let raw_payload = unsafe {
+            // This is safe because we know 'packet.data' is a sub-slice of 'mmap'
+            // and 'mmap' outlives the heap.
+            std::mem::transmute::<&[u8], &'static [u8]>(packet.data.as_ref())
+        };
+
+        match SlicedPacket::from_ethernet(raw_payload) {
             Ok(value) => {
                 match value.transport {
                     Some(etherparse::TransportSlice::Udp(udp)) => {
@@ -44,48 +121,37 @@ fn main() -> Result<(), CustomError> {
                         let Some(quote_packet) = QuotePacketView::try_new(&payload) else {
                             continue;
                         };
-                        // println!("{}.{:06}", ts_sec, ts_usec);
+                        
+                        let ts_sec = packet.timestamp.as_secs();
+                        let ts_usec = packet.timestamp.subsec_micros() as u64;
+
                         let tz_offset_secs = 9 * 3600; // Expecting the packet was captured in a timezone which is KST. 9 hours ahead )
-                        let seconds_today = (ts_sec + tz_offset_secs) % 86400; 
+                        let seconds_today = (ts_sec + tz_offset_secs) % 86400;
 
-                        let packet_total_micros = (seconds_today as u64 * 1_000_000) + (ts_usec as u64);
-                        println!("Packet Time: {}", packet_total_micros);
-                        println!("Issue Code: {:?}", quote_packet.issue_code());
-                        println!("Accept Time: {:?}", quote_packet.accept_time());
-                        let (bid_price1, bid_qty1) = quote_packet.bid(0);
-                        let (bid_price2, bid_qty2) = quote_packet.bid(1);
-                        let (bid_price3, bid_qty3) = quote_packet.bid(2);
-                        let (bid_price4, bid_qty4) = quote_packet.bid(3);
-                        let (bid_price5, bid_qty5) = quote_packet.bid(4);
-                        println!("Bid 1: {:?}@{:?}", bid_price1, bid_qty1);
-                        println!("Bid 2: {:?}@{:?}", bid_price2, bid_qty2);
-                        println!("Bid 3: {:?}@{:?}", bid_price3, bid_qty3);
-                        println!("Bid 4: {:?}@{:?}", bid_price4, bid_qty4);
-                        println!("Bid 5: {:?}@{:?}", bid_price5, bid_qty5);
-                        println!("---");
+                        let normalized_pkt_micros = (seconds_today as u64 * 1_000_000) + (ts_usec as u64);
 
-                        let (ask_price1, ask_qty1) = quote_packet.ask(0);
-                        let (ask_price2, ask_qty2) = quote_packet.ask(1);
-                        let (ask_price3, ask_qty3) = quote_packet.ask(2);
-                        let (ask_price4, ask_qty4) = quote_packet.ask(3);
-                        let (ask_price5, ask_qty5) = quote_packet.ask(4);
-                        println!("Ask 1: {:?}@{:?}", ask_price1, ask_qty1);
-                        println!("Ask 2: {:?}@{:?}", ask_price2, ask_qty2);
-                        println!("Ask 3: {:?}@{:?}", ask_price3, ask_qty3);
-                        println!("Ask 4: {:?}@{:?}", ask_price4, ask_qty4);
-                        println!("Ask 5: {:?}@{:?}", ask_price5, ask_qty5);
-                        println!("---");
-                        // if payload.starts_with(QUOTE_PREFIX) && (d_port == 15515 || d_port == 15516) {
+                        let accept_time = quote_packet.accept_time();
 
-                        //     let data_type = &payload[..2];
-                        //     println!("Data type is : {:?}", std::str::from_utf8(data_type));
+                        let item = Quote {
+                            pkt_time: ts_sec,
+                            accept_time,
+                            quote: quote_packet,
+                        };
 
-                        //     // println!("link: {:?}", value.link);
+                        if args.r {
+                            heap.push(item);
 
-                        //     // println!("link_exts: {:?}", value.link_exts); // contains vlan & macsec
-                        //     // println!("net: {:?}", value.net); // contains ip & arp
-
-                        // }
+                            while let Some(oldest) = heap.peek() {
+                                if normalized_pkt_micros > oldest.accept_time + window_micros {
+                                    let curr_quote = heap.pop().unwrap();
+                                    print_quote(curr_quote);
+                                } else {
+                                    break; // from heap peek
+                                }
+                            }
+                        } else {
+                            print_quote(item);
+                        }
                     }
                     _ => {}
                 }
@@ -93,5 +159,10 @@ fn main() -> Result<(), CustomError> {
             Err(e) => return Err(CustomError::ParseError(format!("Error {:?}", e))),
         }
     }
+
+    while let Some(remaining_quote) = heap.pop() {
+        print_quote(remaining_quote);
+    }
+
     Ok(())
 }
--- src/packet.rs
+++ src/packet.rs
@@ -1,3 +1,5 @@
+use std::cmp::Ordering;
+
 pub const QUOTE_PACKET_SIZE: usize = 215;
 pub const QUOTE_PREFIX: &[u8; 5] = b"B6034";
 pub const ISSUE_CODE_META: &[usize; 2] = &[5, 12]; // OFFSET_VALUE, LENGTH
@@ -9,7 +11,39 @@ pub const ASK_FIRST_QTY_META: &[usize; 2] = &[101, 7];
 pub const LEVEL_STRIDE: usize = 12;
 pub const END_OF_PACKET: usize = 214;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Copy)]
+pub struct Quote<'a> {
+    pub pkt_time: u64,              // for the 3-sec window math
+    pub accept_time: u64,           // storing in heap
+    pub quote: QuotePacketView<'a>, // Zero-copy ref to the mmap
+}
+
+// We want a Min-Heap, so we reverse the ordering of accept_time
+impl<'a> Ord for Quote<'a> {
+    #[inline(always)]
+    fn cmp(&self, other: &Self) -> Ordering {
+        // Reverse standard ordering to make BinaryHeap a Min-Heap
+        other.accept_time.cmp(&self.accept_time)
+    }
+}
+
+impl<'a> PartialOrd for Quote<'a> {
+    #[inline(always)]
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl<'a> PartialEq for Quote<'a> {
+    #[inline(always)]
+    fn eq(&self, other: &Self) -> bool {
+        self.accept_time == other.accept_time
+    }
+}
+
+impl<'a> Eq for Quote<'a> {}
+
+#[derive(Debug, Clone, Copy)]
 pub struct QuotePacketView<'a> {
     raw: &'a [u8],
 }
@@ -17,7 +51,10 @@ pub struct QuotePacketView<'a> {
 impl<'a> QuotePacketView<'a> {
     #[inline(always)]
     pub fn try_new(raw: &'a [u8]) -> Option<Self> {
-        if raw.len() == 215 && &raw[0..5] == QUOTE_PREFIX && raw[END_OF_PACKET] == 0xFF {
+        if raw.len() == QUOTE_PACKET_SIZE
+            && &raw[0..5] == QUOTE_PREFIX
+            && raw[END_OF_PACKET] == 0xFF
+        {
             Some(Self { raw })
         } else {
             None
@@ -33,18 +70,20 @@ impl<'a> QuotePacketView<'a> {
             // acc * 10 shifts the current total to the left by one base-10 digit
             acc = acc * 10 + (b & 0x0F) as u32;
         }
-        
+
         acc
     }
 
     #[inline(always)]
     pub fn issue_code(&self) -> &'a str {
-        std::str::from_utf8(&self.raw[ISSUE_CODE_META[0]..(ISSUE_CODE_META[0] + ISSUE_CODE_META[1])]).unwrap()
+        std::str::from_utf8(
+            &self.raw[ISSUE_CODE_META[0]..(ISSUE_CODE_META[0] + ISSUE_CODE_META[1])],
+        )
+        .unwrap()
     }
 
     #[inline(always)]
     pub fn accept_time(&self) -> u64 {
-        
         // let time_ref1 = std::str::from_utf8(&self.raw[ACCEPT_TIME_META[0]..(ACCEPT_TIME_META[0]+ACCEPT_TIME_META[1])]).unwrap();
         // let hh = time_ref1[0..2].parse::<u64>().unwrap();
         // let mm = time_ref1[2..4].parse::<u64>().unwrap();
@@ -53,8 +92,8 @@ impl<'a> QuotePacketView<'a> {
 
         // // Formula: (hours * 3600 + minutes * 60 + seconds) * 1,000,000 + microseconds
         // let t = (hh * 3600 + mm * 60 + ss) * 1_000_000 + uu;
-        let time_ref = &self.raw[ACCEPT_TIME_META[0]..(ACCEPT_TIME_META[0]+ACCEPT_TIME_META[1])];
-        
+        let time_ref = &self.raw[ACCEPT_TIME_META[0]..(ACCEPT_TIME_META[0] + ACCEPT_TIME_META[1])];
+
         // Explore get_unchecked to unlock more speed
         let hh = (time_ref[0] as u64 - 48) * 10 + (time_ref[1] as u64 - 48);
         let mm = (time_ref[2] as u64 - 48) * 10 + (time_ref[3] as u64 - 48);
@@ -74,9 +113,18 @@ impl<'a> QuotePacketView<'a> {
         let price_start = BID_FIRST_PRICE_META[0] + base;
         let qty_start = BID_FIRST_QTY_META[0] + base;
 
-        let price = Self::parse_ascii_to_u32(&self.raw[price_start..(price_start + BID_FIRST_PRICE_META[1])]);
-        let qty = Self::parse_ascii_to_u32(&self.raw[qty_start..(qty_start + BID_FIRST_QTY_META[1])]);
-        
+        // let raw_price = &self.raw[price_start..(price_start + BID_FIRST_PRICE_META[1])];
+        // let raw_qty = &self.raw[qty_start..(qty_start + BID_FIRST_QTY_META[1])];
+
+        // println!("raw_price: {:?}", std::str::from_utf8(raw_price));
+        // println!("raw_qty: {:?}", std::str::from_utf8(raw_qty));
+
+        let price = Self::parse_ascii_to_u32(
+            &self.raw[price_start..(price_start + BID_FIRST_PRICE_META[1])],
+        );
+        let qty =
+            Self::parse_ascii_to_u32(&self.raw[qty_start..(qty_start + BID_FIRST_QTY_META[1])]);
+
         (price, qty)
     }
 
@@ -86,9 +134,12 @@ impl<'a> QuotePacketView<'a> {
         let price_start = ASK_FIRST_PRICE_META[0] + base;
         let qty_start = ASK_FIRST_QTY_META[0] + base;
 
-        let price = Self::parse_ascii_to_u32(&self.raw[price_start..(price_start + ASK_FIRST_PRICE_META[1])]);
-        let qty = Self::parse_ascii_to_u32(&self.raw[qty_start..(qty_start + ASK_FIRST_QTY_META[1])]);
-        
+        let price = Self::parse_ascii_to_u32(
+            &self.raw[price_start..(price_start + ASK_FIRST_PRICE_META[1])],
+        );
+        let qty =
+            Self::parse_ascii_to_u32(&self.raw[qty_start..(qty_start + ASK_FIRST_QTY_META[1])]);
+
         (price, qty)
     }
 }
